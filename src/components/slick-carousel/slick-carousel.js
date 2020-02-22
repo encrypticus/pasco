a13f34b3c2390b075483d6e2b8eddc989c6bd468
encrypticus <encrypticus@gmail.com>
2020-02-22 18:12:17 +0600

    src/components/slick-carousel/slick-carousel.js: мелкая правка.

--- src/components/slick-carousel/slick-carousel.js
+++ src/components/slick-carousel/slick-carousel.js
@@ -2,7 +2,17 @@ import 'jquery';
 import 'slick-carousel/slick/slick';
 import './slick-carousel.scss';
 
+const prevArrow = `
+  <button type="button" class="slick-carousel__btn slick-carousel__btn_prev" aria-label="Previous">
+    <span class="mi mi-keyboard-arrow-left"></span>
+  </button>`;
+
+const nextArrow = `
+  <button type="button" class="slick-carousel__btn slick-carousel__btn_next" aria-label="Next">
+    <span class="mi mi-keyboard-arrow-right"></span>
+  </button>`;
+
 $('.slick-carousel').slick({
-  prevArrow: '<button type="button" class="slick-carousel__btn slick-carousel__btn_prev" aria-label="Previous"><span class="mi mi-keyboard-arrow-left"></span></button>',
-  nextArrow: '<button type="button" class="slick-carousel__btn slick-carousel__btn_next" aria-label="Next"><span class="mi mi-keyboard-arrow-right"></span></button>',
+  prevArrow,
+  nextArrow,
 });
