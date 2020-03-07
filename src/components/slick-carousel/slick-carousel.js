1f7920c9d99b6e226149a90ec272d96c7ec214bc
encrypticus <encrypticus@gmail.com>
2020-03-07 16:15:07 +0600

    component slick-carousel: рефакторинг.

--- src/components/slick-carousel/slick-carousel.js
+++ src/components/slick-carousel/slick-carousel.js
@@ -2,17 +2,17 @@ import 'jquery';
 import 'slick-carousel/slick/slick';
 import './slick-carousel.scss';
 
-const prevArrow = `
+const prevArrowSmall = `
   <button type="button" class="slick-carousel__btn slick-carousel__btn_prev" aria-label="Previous">
     <span class="mi mi-keyboard-arrow-left"></span>
   </button>`;
 
-const nextArrow = `
+const nextArrowSmall = `
   <button type="button" class="slick-carousel__btn slick-carousel__btn_next" aria-label="Next">
     <span class="mi mi-keyboard-arrow-right"></span>
   </button>`;
 
-$('.slick-carousel').slick({
-  prevArrow,
-  nextArrow,
+$('.slick-carousel.slick-carousel_btn_sm').slick({
+  prevArrow: prevArrowSmall,
+  nextArrow: nextArrowSmall,
 });
